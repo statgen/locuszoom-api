98a523632e4a4016c6790c5ca39e9571633f8d1e
Ryan Welch <welchr@umich.edu>
2017-01-20 19:20:21 -0500

    Fix for relative path to config files

--- portalapi/__init__.py
+++ portalapi/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.cors import CORS
 from flask.ext.cache import Cache
 
 # Create flask app
-app = Flask(__name__,instance_path="/home/portaldev/lzapi_dev/etc/",instance_relative_config=True)
+app = Flask(__name__)
 
 # Which config to use
 mode = os.environ.get("PORTALAPI_MODE")
@@ -12,13 +12,13 @@ if mode is None:
   raise Exception("No API mode designated. Set the PORTALAPI_MODE environment variable to 'dev' or 'prod'")
 elif mode == "dev":
   print "Starting with development config..."
-  app.config.from_pyfile("config-dev.py")
+  app.config.from_pyfile("../etc/config-dev.py")
 elif mode == "prod":
   print "Starting with production config..."
-  app.config.from_pyfile("config-prod.py")
+  app.config.from_pyfile("../etc/config-prod.py")
 elif mode == "quick":
   print "Starting with quick config..."
-  app.config.from_pyfile("config-quick.py")
+  app.config.from_pyfile("../etc/config-quick.py")
 else:
   raise Exception("Unrecognized value for PORTALAPI_MODE: " + mode)
 
