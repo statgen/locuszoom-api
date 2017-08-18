134aa6315d6fb1448b7232351022071646db92df
test-jenkins-locuszoom <31143197+test-jenkins-locuszoom@users.noreply.github.com>
2017-08-18 16:59:49 -0400

    Update unused_port.py

--- bin/unused_port.py
+++ bin/unused_port.py
@@ -2,10 +2,4 @@
 from __future__ import print_function
 import psutil, random
 
-used_ports = set([x.laddr[1] for x in psutil.net_connections()])
-while 1:
-  port = random.randint(1024,65535)
-  if port not in used_ports:
-    print(port)
-    break
-
+print("-99")
