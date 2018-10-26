077bf5ac7cae157dc29fc729a9a82fe514e82204
Ryan Welch <welchr@umich.edu>
2018-10-26 19:08:39 -0400

    Add release and environment tracking to Sentry
    Release is the SHA of the current version of the code. They say it is
    global across the organization, so it should be prefixed with something
    project-specific (like locuszoom-api), but in practice I don't see that
    happening in the Sentry UI.
    
    Environment is just production, staging/dev, etc. You can filter by
    environment in the very upper-right hand corner of the UI.

--- locuszoom/api/sentry.py
+++ locuszoom/api/sentry.py
@@ -1,11 +1,22 @@
+import os
 from raven.contrib.flask import Sentry
+from raven.versioning import fetch_git_sha
 
 def init_app(app):
   # Start logging errors
   sentry = None
+
+  sha = fetch_git_sha(os.path.join(app.root_path,"../../"))
+  release = f"locuszoom-api@{sha}"
+
   if "SENTRY_DSN" in app.config:
     print("Sentry DSN found, attaching to app")
-    sentry = Sentry(app,dsn=app.config["SENTRY_DSN"],register_signal=False,wrap_wsgi=False)
+    app.config["SENTRY_CONFIG"] = {
+      "dsn": app.config["SENTRY_DSN"],
+      "release": release,
+      "environment": app.config["LZAPI_MODE"]
+    }
+    sentry = Sentry(app,register_signal=False,wrap_wsgi=False)
   else:
     print("Warning: Sentry DSN not found, skipping")
 
