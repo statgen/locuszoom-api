752d45ae0f8a7daaf2628dd75a766abff469d0c2
Ryan Welch <welchr@umich.edu>
2021-01-11 23:54:28 -0500

    Add variant_format option to GWAS catalog endpoint
    Pass 'variant_format=colons' to get Broad style variants, e.g. '10:1414141:A:C'.

--- tests/test_annotation_gwascatalog.py
+++ tests/test_annotation_gwascatalog.py
@@ -25,3 +25,33 @@ def test_annotation_gwascatalog(client):
 
   for k in "id variant rsid chrom pos ref alt trait trait_group risk_allele risk_frq log_pvalue or_beta genes pmid pubdate first_author study".split():
     assert k in result_data["data"]
+
+def test_annotation_gwascatalog_colons(client):
+  # Check metadata endpoint
+  resp = client.get("/v1/annotation/gwascatalog/")
+  assert resp.status_code == 200
+
+  # Check results endpoint
+  data = resp.json
+  test_id = data["data"]["id"][0]
+  params = {
+    "filter": "id in {} and rsid eq 'rs7903146'".format(test_id),
+    "limit": 1,
+    "sort": "pos",
+    "format": "objects",
+    "variant_format": "colons"
+  }
+  results = client.get("/v1/annotation/gwascatalog/results/",query_string=params)
+
+  assert results.status_code == 200
+
+  result_data = results.json
+  assert "data" in result_data
+  assert len(result_data["data"]) > 0
+
+  for entry in result_data["data"]:
+    for k in "id variant rsid chrom pos ref alt trait trait_group risk_allele risk_frq log_pvalue or_beta genes pmid pubdate first_author study".split():
+      assert k in entry
+
+    assert '_' not in entry["variant"]
+    assert '/' not in entry["variant"]
