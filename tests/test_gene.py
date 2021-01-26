ca622925892d1bcb11ecf525386e94c213397a6e
Ryan Welch <welchr@umich.edu>
2021-01-25 21:57:13 -0500

    Test for gene API using recommended source

--- tests/test_gene.py
+++ tests/test_gene.py
@@ -49,6 +49,54 @@ def test_gene(client):
       assert tx["strand"] in ("+","-")
       assert len(tx["exons"]) > 0
 
+def test_gene_recommended_source(client):
+  params = {
+    "filter": "chrom eq '16' and start le 57022881 and end ge 56985060",
+    "build": "GRCh37"
+  }
+  resp = client.get("/v1/annotation/genes/",query_string=params)
+
+  assert resp.status_code == 200
+  assert len(resp.json["data"]) > 0
+
+  # Check format of gene
+  for gene in resp.json["data"]:
+    for key in ("chrom","end","exons","gene_id","gene_name","start","strand","transcripts"):
+      assert key in gene
+
+    assert isinstance(gene["chrom"],string_types)
+    assert isinstance(gene["gene_id"],string_types)
+    assert isinstance(gene["gene_name"],string_types)
+    assert gene["strand"] in ("+","-")
+    assert isinstance(gene["end"],int)
+    assert isinstance(gene["start"],int)
+    assert isinstance(gene["gene_type"],string_types)
+
+    assert len(gene["exons"]) > 0
+
+    for exon in gene["exons"]:
+      for key in ("chrom","end","exon_id","start","strand"):
+        assert key in exon
+
+      assert isinstance(exon["exon_id"],string_types)
+      assert isinstance(exon["chrom"],string_types)
+      assert isinstance(exon["end"],int)
+      assert isinstance(exon["start"],int)
+      assert exon["strand"] in ("+","-")
+
+    assert len(gene["transcripts"]) > 0
+
+    for tx in gene["transcripts"]:
+      for key in ("chrom","end","exons","start","strand","transcript_id"):
+        assert key in tx
+
+      assert isinstance(tx["transcript_id"],string_types)
+      assert isinstance(tx["chrom"],string_types)
+      assert isinstance(tx["end"],int)
+      assert isinstance(tx["start"],int)
+      assert tx["strand"] in ("+","-")
+      assert len(tx["exons"]) > 0
+
 def test_gene_chrom_null(client):
   params = {
     "filter": "source in 2 and chrom eq 'null' and start le 57022881 and end ge 56985060"
