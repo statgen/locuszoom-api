5c6162124b2519bb84c7625fb66f950f8e11ce98
Ryan Welch <welchr@umich.edu>
2021-10-24 15:19:25 -0400

    Add password arg to waitdb

--- tests/data/pytest_waitdb.py
+++ tests/data/pytest_waitdb.py
@@ -17,6 +17,7 @@ def get_settings():
   p.add_argument("--port")
   p.add_argument("--host")
   p.add_argument("--user")
+  p.add_argument("--password")
   p.add_argument("--retries", default=10, help="Number of times to retry database.")
   p.add_argument("--interval", default=3, help="Time to wait between retries.")
 
@@ -25,6 +26,7 @@ def get_settings():
   args.host = args.host if args.host is not None else os.environ["POSTGRES_HOST"]
   args.port = args.port if args.port is not None else os.environ["POSTGRES_PORT"]
   args.user = args.user if args.user is not None else os.environ["POSTGRES_USER"]
+  args.password = args.password if args.password is not None else os.environ.get("POSTGRES_PASSWORD")
   args.connect_db = args.connect_db if args.connect_db is not None else os.environ["POSTGRES_CONNECT_DB"]
 
   return args
@@ -37,7 +39,7 @@ if __name__ == "__main__":
   for _ in range(args.retries):
     try:
       print("Trying to connect to postgres to check if database is up...")
-      init_con = psycopg2.connect(database=args.connect_db, host=args.host, port=args.port, user=args.user)
+      init_con = psycopg2.connect(database=args.connect_db, host=args.host, port=args.port, user=args.user, password=args.password)
     except:
       print("Database did not respond, waiting to retry...")
       time.sleep(args.interval)
