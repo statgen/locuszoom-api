8134b6d772fd89f8264a62d1e74c9be7e28075ca
Ryan Welch <welchr@umich.edu>
2021-10-26 15:52:12 -0400

    Add app pytest fixture, remove session scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,11 @@ import pytest
 from flask import url_for
 from locuszoom.api import create_app
 
-@pytest.fixture(scope="session")
-def client():
-  app = create_app()
+@pytest.fixture
+def app():
+  return create_app()
+
+@pytest.fixture
+def client(app):
   client = app.test_client()
   return client
